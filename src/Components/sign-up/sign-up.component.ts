c37813b77d16ad23638910f2ef5803055e5f60a8
Yasser <yasserhhh161@gmail.com>
2024-05-03 22:35:47 +0300

    Email and Password validation

--- src/Components/sign-up/sign-up.component.ts
+++ src/Components/sign-up/sign-up.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IUser } from '../../Models/iuser';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-
+ user : IUser = {usermail: '' , password:'', confirmpassword:''}
 }
---
+++ src/Models/iuser.ts
@@ -0,0 +1,5 @@
+export interface IUser {
+    usermail : string 
+    password : string
+    confirmpassword : string
+}
