b97a5866bd931e3edefd5a594cab7e23054a1340
Yasser <yasserhhh161@gmail.com>
2024-05-04 18:48:56 +0300

    Passenger and Document details

--- src/Components/sign-up/sign-up.component.ts
+++ src/Components/sign-up/sign-up.component.ts
@@ -10,5 +10,43 @@ import { IUser } from '../../Models/iuser';
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
- user : IUser = {usermail: '' , password:'', confirmpassword:''}
+ toggleselect:boolean = false;
+ toggledocument:boolean = false;
+ togglecountry:boolean = false;
+  selectedDay: string = '';
+  selectedMonth: string = '';
+  selectedYear: string = '';
+  selectedexpireDay: string = '';
+  selectedexpireMonth: string = '';
+  selectedexpireYear: string = '';
+  isolderenough: boolean = true;
+  notexpired: boolean = true;
+ user : IUser = {usermail: '' , password:'', confirmpassword:'' , title:'' , firstname:'' , lastname:'' , birthdate : new Date(`${this.selectedDay}/${this.selectedMonth}/${this.selectedYear}`),
+                  documenttype: '', documentnumber : '' , issuingcountry : '' ,documentexpirydate : new Date(`${this.selectedexpireDay}/${this.selectedexpireMonth}/${this.selectedexpireYear}`)}
+ opened(){
+  this.toggleselect =true;
+ }
+ opendocument(){
+  this.toggledocument = true;
+ }
+ opencountry(){
+  this.togglecountry = true;
+ }
+ onSubmit(event : any , signupform:any){
+  const currentDate = new Date();
+  const expireDate = new Date(parseInt(this.selectedexpireYear), parseInt(this.selectedexpireMonth) - 1, parseInt(this.selectedexpireDay));
+  if((new Date().getFullYear() - parseInt(this.selectedYear)) < 18){
+    this.isolderenough = false;
+    event.stopPropagation();
+  }
+  else if(signupform.value.documenttype == 'Passport' && expireDate.toISOString() < currentDate.toISOString()){
+    this.notexpired =false;
+    event.stopPropagation();
+  }
+  else{
+    this.isolderenough =true;
+    this.notexpired =true;
+    console.log(signupform.value);
+  }
+ }
 }
--- src/Models/iuser.ts
+++ src/Models/iuser.ts
@@ -1,5 +1,15 @@
+import { Data } from "@angular/router";
+
 export interface IUser {
-    usermail : string 
-    password : string
-    confirmpassword : string
+    usermail : string,
+    password : string,
+    confirmpassword : string,
+    title : string,
+    firstname : string,
+    lastname : string,
+    birthdate : Data,
+    documenttype : string,
+    documentnumber : string,
+    issuingcountry? : string,
+    documentexpirydate?: Data
 }
